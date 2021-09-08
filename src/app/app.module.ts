9a553b86cda8481a03810dbb87970ca20c0ca757
unknown <sanjeevdandin@gmail.com>
2021-09-08 22:39:29 +0530

    created registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
